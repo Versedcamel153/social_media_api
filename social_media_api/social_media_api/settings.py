7b0eca55103ea01e391d38790bdcacc28ae61c5a
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-19 20:05:03 +0000

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(default=os.environment.get('DATABASE_URL'))
+        dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 }
 
